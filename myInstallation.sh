0224f703b711a6c12b8485519d87c4c78dfbadf3
SeayPrem <seayprem@hotmail.com>
2021-06-14 21:46:45 +0700

    Add package homebrew

--- myInstallation.sh
+++ myInstallation.sh
@@ -29,4 +29,12 @@ git clone https://github.com/seayprem/.files
 cp .file/.vimrc ~/.vimrc
 cp .file/.tmux.conf ~/.tmux.conf
 
+# Home Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+brew install hello
