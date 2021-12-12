251a9d8d8d075f5d888808efe7c80161619e303a
SeayPrem <seayprem@hotmail.com>
2021-12-12 12:47:33 +0700

    added more package

--- myInstallation.sh
+++ myInstallation.sh
@@ -10,7 +10,7 @@ sudo apt update
 sudo apt upgrade -y
 
 # generation package
-sudo apt install build-essential cmake
+sudo apt install build-essential cmake libssl-dev clang
 sudo apt install vim
 sudo apt install tmux
 sudo apt install zsh
